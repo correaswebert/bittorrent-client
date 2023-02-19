be3f943a7462e5d8c88a3a1c674111e143c787ef
Swebert Correa <correaswebert@gmail.com>
2023-02-19 23:04:03 +0530

    [WIP] Tracker http working
    Signed-off-by: Swebert Correa <correaswebert@gmail.com>

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import hashlib
 from torrent.parser import bdecode, bencode
-from torrent.tracker import http_tracker
+from torrent.tracker import get_peer_list
 
 if __name__ == "__main__":
     with open("data/small.torrent", "rb") as tfile:
         tdata = tfile.read()
     
     metainfo = bdecode(tdata)
+    metainfo_info = bencode(metainfo["info"])
+    metainfo_info_hash = hashlib.sha1(metainfo_info).digest()
 
-    responses = http_tracker(metainfo)
+    responses = get_peer_list(metainfo, metainfo_info_hash)
     for response in responses:
-        print(response.text)
-        # print(bdecode(response.text))
+        print(bdecode(response.text))
 
