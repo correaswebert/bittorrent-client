a40ae234268d3b9063b95f238f6f205fc4c1fce5
Swebert Correa <correaswebert@gmail.com>
2023-02-21 00:12:59 +0530

    [WIP] Use Protocols to implement asyncio
    Signed-off-by: Swebert Correa <correaswebert@gmail.com>

--- torrent/tracker/udp_tracker.py
+++ torrent/tracker/udp_tracker.py
@@ -6,7 +6,7 @@ class UDPTrackerProtocol:
     def __init__(self, message, on_con_lost):
         self.message = message
         self.on_con_lost = on_con_lost
-        self.transport = None
+        self.transport = ...
 
     def connection_made(self, transport):
         self.transport = transport
@@ -26,11 +26,26 @@ class UDPTrackerProtocol:
         print("Connection closed")
         self.on_con_lost.set_result(True)
 
-    async def tracker_connect(self):
-        ...
 
-    async def tracker_announce(self):
-        ...
+async def tracker_connect():
+    ...
 
-    def get_peer_list(self):
-        ...
+
+async def tracker_announce():
+    ...
+
+
+async def get_peer_list():
+    loop = asyncio.get_running_loop()
+
+    on_con_lost = loop.create_future()
+    message = "Hello World!"
+
+    transport, protocol = await loop.create_datagram_endpoint(
+        lambda: UDPTrackerProtocol(message, on_con_lost),
+        remote_addr=('127.0.0.1', 9999))
+
+    try:
+        await on_con_lost
+    finally:
+        transport.close()
