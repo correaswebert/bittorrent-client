5ee5bd8c2be565de855ac8825f88150d8a344fca
Swebert Correa <correaswebert@gmail.com>
2023-02-21 01:04:46 +0530

    Get peer addresses from HTTP tracker
    Signed-off-by: Swebert Correa <correaswebert@gmail.com>

--- torrent/tracker/http_tracker.py
+++ torrent/tracker/http_tracker.py
@@ -1,6 +1,9 @@
+import struct
 from urllib.parse import urlencode
 import httpx
 import asyncio
+
+from torrent.parser import bdecode
 
 
 async def get_peer_list_http(http_requests, payload: dict[str, bytes | int]):
@@ -9,4 +12,28 @@ async def get_peer_list_http(http_requests, payload: dict[str, bytes | int]):
         coros = [client.get(f"{url}?{urlencoded_payload}") for url in http_requests]
         responses = await asyncio.gather(*coros)
 
-    return responses
+    for benc_response in responses:
+        response = bdecode(benc_response.text)
+
+        if "failure reason" in response:
+            raise Exception(response["failure reason"])
+
+        enc_peers: list[bytes] | bytes = response["peers"]
+        peers = []
+
+        if isinstance(enc_peers, list):
+            peers = [(peer["ip"], peer["port"]) for peer in enc_peers]
+
+        elif isinstance(enc_peers, bytes):
+            while enc_peers:
+                peer_addr = enc_peers[:6]
+                enc_peers = enc_peers[6:]
+
+                peer_ip, peer_port = struct.unpack("!4sH", peer_addr)
+                peer_ip = ".".join(str(i) for i in peer_ip)
+                peers.append((peer_ip, peer_port))
+
+        else:
+            print(f"Invalid peer list {enc_peers=}")
+
+    return peers
