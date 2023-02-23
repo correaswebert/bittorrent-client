a9f802c098a65bbac4b5cedfcaefd5c7c199fca6
Swebert Correa <correaswebert@gmail.com>
2023-02-24 01:35:24 +0530

    [WIP] Create metainfo class
    Signed-off-by: Swebert Correa <correaswebert@gmail.com>

--- main.py
+++ main.py
@@ -1,25 +1,49 @@
 import hashlib
 from torrent.parser import bdecode, bencode
 from torrent.tracker import get_peers
+import uuid
 
+from torrent.common.metainfo import Metainfo
 import click
-from torrent.utils.logger import log, stream_logs, set_log_level
+from torrent.util.logger import log, stream_logs, set_log_level
 
 @click.command()
-@click.option("--verbose", help="Set logging verbosity")
-def main(verbose):
+@click.option("-v", "--verbose", help="Set logging verbosity")
+@click.argument("torrent", type=click.Path(exists=True))
+def main(verbose: str, torrent: str):
+    """Download torrent"""
+
     set_log_level(verbose)
     stream_logs()
 
-    with open("data/small.torrent", "rb") as tfile:
+    with open(torrent, "rb") as tfile:
         tdata = tfile.read()
     
-    metainfo = bdecode(tdata)
-    metainfo_info = bencode(metainfo["info"])
-    metainfo_info_hash = hashlib.sha1(metainfo_info).digest()
+    metainfo_data = bdecode(tdata)
+    # for file in metainfo_data["info"]["files"]:
+    #     print(file)
 
-    peers = get_peers(metainfo, metainfo_info_hash)
-    log.info(peers)
+    metainfo = Metainfo()
+    if "name" in metainfo_data["info"]:
+        metainfo.name = metainfo_data["info"]["name"]
+    metainfo.piece_length = metainfo_data["info"]["piece length"]
+    metainfo.pieces = metainfo_data["info"]["pieces"]
+    metainfo.pieces = metainfo_data["info"]["pieces"]
+
+    if "files" in metainfo_data["info"]:
+        for file in metainfo_data["info"]["files"]:
+            metainfo.files.append((file["length"], file["path"][0]))
+    else:
+        metainfo.files.append((metainfo_data["info"]["length"], metainfo.name))
+    print(metainfo)
+
+    # metainfo_info = bencode(metainfo["info"])
+    # metainfo_info_hash = hashlib.sha1(metainfo_info).digest()
+
+    # peers = get_peers(metainfo, metainfo_info_hash)
+    # print(peers)
+
+    # uuid.uuid4()
 
 
 if __name__ == "__main__":
