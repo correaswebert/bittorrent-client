8d5cf46ff6b75d27390f7c1f45d235e411ce34f9
Swebert Correa <correaswebert@gmail.com>
2023-02-24 02:11:25 +0530

    Metainfo parser torrent files
    Signed-off-by: Swebert Correa <correaswebert@gmail.com>

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import hashlib
 from torrent.parser import bdecode, bencode
 from torrent.tracker import get_peers
-import uuid
 
 from torrent.common.metainfo import Metainfo
 import click
@@ -16,34 +15,12 @@ def main(verbose: str, torrent: str):
     set_log_level(verbose)
     stream_logs()
 
-    with open(torrent, "rb") as tfile:
-        tdata = tfile.read()
-    
-    metainfo_data = bdecode(tdata)
-    # for file in metainfo_data["info"]["files"]:
-    #     print(file)
+    metainfo = Metainfo(torrent)
+    print(metainfo
+          )
 
-    metainfo = Metainfo()
-    if "name" in metainfo_data["info"]:
-        metainfo.name = metainfo_data["info"]["name"]
-    metainfo.piece_length = metainfo_data["info"]["piece length"]
-    metainfo.pieces = metainfo_data["info"]["pieces"]
-    metainfo.pieces = metainfo_data["info"]["pieces"]
-
-    if "files" in metainfo_data["info"]:
-        for file in metainfo_data["info"]["files"]:
-            metainfo.files.append((file["length"], file["path"][0]))
-    else:
-        metainfo.files.append((metainfo_data["info"]["length"], metainfo.name))
-    print(metainfo)
-
-    # metainfo_info = bencode(metainfo["info"])
-    # metainfo_info_hash = hashlib.sha1(metainfo_info).digest()
-
-    # peers = get_peers(metainfo, metainfo_info_hash)
+    # peers = get_peers(metainfo, metainfo.info_hash)
     # print(peers)
-
-    # uuid.uuid4()
 
 
 if __name__ == "__main__":
--- torrent/peer/connect.py
+++ torrent/peer/connect.py
@@ -1,47 +1,23 @@
 import asyncio
 import uuid
 
+from torrent.common.metainfo import Metainfo
+
 class TorrentNode:
-    def __init__(self):
-        self.peers = []
+    def __init__(self, metainfo: Metainfo):
+        self.peers: list[tuple[asyncio.StreamReader, asyncio.StreamWriter]] = []
         self.id = b"-CS0001-123456789012" # TODO: generate this
+        self.pending_peices = len(metainfo.pieces)
 
     
-    def connect(self, peer_addr: tuple[str, int]):
+    async def connect(self, peer_addr: tuple[str, int]):
         reader, writer = await asyncio.open_connection(*peer_addr)
-
-    def establish_connection(peer_list, meta_info, info_hash, peer_id, download_location):
-        global DOWNLOADED_PIECES, downloading_file,  REMAINING_PIECES
-
-        if download_location is None:
-            download_location = "data/downloaded/"
-
-        # # progressbar = ProgressBar(
-        # "Downloading", max = len(meta_info['info']['pieces']))
-        downloading_file = open(
-            f"{download_location}/{meta_info['info']['name']}", "wb")
-
-        REMAINING_PIECES = len(meta_info['info']['pieces'])
-        DOWNLOADED_PIECES = [0] * REMAINING_PIECES
-
-        # connect to all peers and download pieces
-        for peer_addr in peer_list:
-            Thread(target=connect,
-                args=(peer_addr, info_hash, peer_id, meta_info)).start()
-            # connect(peer_addr, info_hash, peer_id, meta_info)
-
-        if not REMAINING_PIECES:
-            log.critical("File downloaded!")
-
-        # progressbar.finish()
-
-        # TODO: implement rarest first approach for downloading a piece
-        # pp.pprint(bitfield)
+        self.peers.append((reader, writer))
 
             
 class TorrentDownload:
     def __init__(self, file: str):
         self.file: str = file
 
-if __name__ == "__main__"
+if __name__ == "__main__":
     ...
